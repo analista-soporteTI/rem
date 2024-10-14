f2e475d5154d51878000742e9bd9e9a8e1b6ce13
Daniel Cordero <daniel@corderodev.com>
2024-10-14 13:28:00 -0300

    feat: add unique code generator
    Generates a unique code with prefix, letters and numbers for REM requests. Currently using this code only in mail subjects

---
+++ src/utils/generateUniqueCode.ts
@@ -0,0 +1,13 @@
+import { v4 as uuidv4 } from 'uuid'
+
+export const generateUniqueCode = (prefix: string) => {
+  const uuid = uuidv4()
+
+  const letters = uuid.replace(/[^a-zA-Z]/g, '')
+  const numbers = uuid.replace(/[^0-9]/g, '')
+
+  const twoLetters = letters.slice(0, 2).toUpperCase()
+  const fourNumbers = numbers.slice(0, 4)
+
+  return `${prefix}_${twoLetters}${fourNumbers}`
+}
