d01181d6f4a03f5b7b97c60f5aca3a362f9e7397
Daniel Cordero <daniel@corderodev.com>
2024-10-14 14:37:42 -0300

    feat: add email template of the REM information
    Includes product information and user inputs

---
+++ src/components/templates/EmailSendRem.tsx
@@ -0,0 +1,59 @@
+interface Product {
+  Product: string
+  SKU: string
+  Stock: number
+  Imagen: string
+  quantity: number
+}
+
+interface EmailSendRemProps {
+  name: string
+  email: string
+  date: string
+  ceco: string
+  message: string
+  products: Product[]
+}
+
+export const EmailSendRem: React.FC<Readonly<EmailSendRemProps>> = ({
+  name,
+  email,
+  date,
+  ceco,
+  message,
+  products
+}) => (
+  <div>
+    <section>
+      <h1>Solicitud REM</h1>
+      <p>
+        <strong>Solicitante:</strong> {name}
+      </p>
+      <p>
+        <strong>Fecha solicitada de entrega:</strong> {date}
+      </p>
+      <p>
+        <strong>Correo:</strong> {email}
+      </p>
+      <p>
+        <strong>CeCo:</strong> {ceco}
+      </p>
+      {message && (
+        <p>
+          <strong>Comentarios:</strong> <br />
+          {message}
+        </p>
+      )}
+    </section>
+    <ul className='border-t border-border mt-2'>
+      {products.map(product => (
+        <li key={product.SKU}>
+          <strong>Producto:</strong> {product.Product} <br />
+          <strong>SKU:</strong> {product.SKU} <br />
+          <strong>Stock disponible:</strong> {product.Stock} <br />
+          <strong>Cantidad solicitada:</strong> {product.quantity} <br />
+        </li>
+      ))}
+    </ul>
+  </div>
+)
