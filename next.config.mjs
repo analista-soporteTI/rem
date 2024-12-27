07dee5204801898f74b50cab422da9990d7e765b
Daniel Cordero <daniel@corderodev.com>
2024-12-27 13:12:26 -0300

    feat: add environment variables for Turso and Algolia in Next.js setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    TURSO_DATABASE_URL: process.env.TURSO_DATABASE_URL,
+    TURSO_AUTH_TOKEN: process.env.TURSO_AUTH_TOKEN,
+    TURSO_DATABASE_NAME: process.env.TURSO_DATABASE_NAME,
+    TURSO_ORGANIZATION_NAME: process.env.TURSO_ORGANIZATION_NAME,
+    NEXT_PUBLIC_ALGOLIA_APP_ID: process.env.NEXT_PUBLIC_ALGOLIA_APP_ID,
+    NEXT_PUBLIC_ALGOLIA_SEARCH_KEY: process.env.NEXT_PUBLIC_ALGOLIA_SEARCH_KEY,
+    NEXT_PUBLIC_ALGOLIA_INDEX_NAME: process.env.NEXT_PUBLIC_ALGOLIA_INDEX_NAME,
+    ALGOLIA_API_KEY: process.env.ALGOLIA_API_KEY,
+    RESEND_API_KEY: process.env.RESEND_API_KEY
+  },
   images: {
-    domains: ['res.cloudinary.com']
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com'
+      }
+    ]
   },
   async headers () {
     return [
