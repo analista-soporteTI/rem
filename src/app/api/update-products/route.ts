b4309295f50fb641195816e160e3835a883a7424
Daniel Cordero <daniel@corderodev.com>
2024-12-30 13:28:40 -0300

    feat: implement API endpoint for product updates with scheduled cron job
    Create an API endpoint to update products, integrated with a cron job in Vercel configuration for automated updates

---
+++ src/app/api/update-products/route.ts
@@ -0,0 +1,16 @@
+import { updateProducts } from '@/lib/db/insertProducts'
+
+export const config = {
+  runtime: 'edge'
+}
+
+export default async function updateProductsDb () {
+  try {
+    await updateProducts()
+    return new Response('Products updated successfully', { status: 200 })
+  } catch (error) {
+    return new Response('Error updating products: ' + error, {
+      status: 500
+    })
+  }
+}
