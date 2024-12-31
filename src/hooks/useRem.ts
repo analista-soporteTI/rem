45891bb6e1f0e280f4ca9280258219db16add4ea
Daniel Cordero <daniel@corderodev.com>
2024-12-31 10:27:27 -0300

    refactor: remove use of local storage in rem store

--- src/hooks/useRem.ts
+++ src/hooks/useRem.ts
@@ -2,7 +2,6 @@
 
 import { Rem } from '@/types/rem'
 import { create } from 'zustand'
-import { persist, createJSONStorage } from 'zustand/middleware'
 
 type RemId = string
 
@@ -24,93 +23,76 @@ interface RemsActions {
   getPinnedRems: () => Rem[]
 }
 
-const useRemsStore = create<RemsState & RemsActions>()(
-  persist(
-    (set, get) => ({
-      rems: {},
-      pinnedRems: new Set<RemId>(),
+const useRemsStore = create<RemsState & RemsActions>()((set, get) => ({
+  rems: {},
+  pinnedRems: new Set<RemId>(),
 
-      addRems: newRems =>
-        set(state => ({
-          rems: {
-            ...state.rems,
-            ...newRems.reduce((acc, rem) => {
-              if (!state.rems[rem.rem_code]) {
-                acc[rem.rem_code] = rem
-              }
-              return acc
-            }, {} as Record<RemId, Rem>)
-          }
-        })),
-
-      updateRem: rem =>
-        set(state => ({
-          rems: {
-            ...state.rems,
-            [rem.rem_code]: {
-              ...state.rems[rem.rem_code],
-              ...rem
-            }
-          }
-        })),
-
-      removeRem: remId =>
-        set(state => {
-          // eslint-disable-next-line @typescript-eslint/no-unused-vars
-          const { [remId]: _, ...rest } = state.rems
-          const newPinnedRems = new Set(state.pinnedRems)
-          newPinnedRems.delete(remId)
-          return { rems: rest, pinnedRems: newPinnedRems }
-        }),
-
-      clearRems: () => set({ rems: {}, pinnedRems: new Set<RemId>() }),
-
-      setRems: rems =>
-        set({
-          rems: rems.reduce((acc, rem) => {
+  addRems: newRems =>
+    set(state => ({
+      rems: {
+        ...state.rems,
+        ...newRems.reduce((acc, rem) => {
+          if (!state.rems[rem.rem_code]) {
             acc[rem.rem_code] = rem
-            return acc
-          }, {} as Record<RemId, Rem>)
-        }),
-
-      pinRem: remId =>
-        set(state => ({
-          pinnedRems: new Set(state.pinnedRems).add(remId)
-        })),
-
-      unpinRem: remId =>
-        set(state => {
-          const newPinnedRems = new Set(state.pinnedRems)
-          newPinnedRems.delete(remId)
-          return { pinnedRems: newPinnedRems }
-        }),
-
-      isPinned: remId => get().pinnedRems.has(remId),
-
-      getAllRems: () => Object.values(get().rems),
-
-      getPinnedRems: () => {
-        const { rems, pinnedRems } = get()
-        return Array.from(pinnedRems)
-          .map(id => rems[id])
-          .filter(Boolean)
+          }
+          return acc
+        }, {} as Record<RemId, Rem>)
       }
+    })),
+
+  updateRem: rem =>
+    set(state => ({
+      rems: {
+        ...state.rems,
+        [rem.rem_code]: {
+          ...state.rems[rem.rem_code],
+          ...rem
+        }
+      }
+    })),
+
+  removeRem: remId =>
+    set(state => {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const { [remId]: _, ...rest } = state.rems
+      const newPinnedRems = new Set(state.pinnedRems)
+      newPinnedRems.delete(remId)
+      return { rems: rest, pinnedRems: newPinnedRems }
     }),
-    {
-      name: 'rems-storage',
-      storage: createJSONStorage(() => localStorage),
-      partialize: state => ({
-        rems: state.rems,
-        pinnedRems: Array.from(state.pinnedRems)
-      }),
-      merge: (persistedState: any, currentState) => ({
-        ...currentState,
-        ...persistedState,
-        pinnedRems: new Set(persistedState.pinnedRems || [])
-      })
-    }
-  )
-)
+
+  clearRems: () => set({ rems: {}, pinnedRems: new Set<RemId>() }),
+
+  setRems: rems =>
+    set({
+      rems: rems.reduce((acc, rem) => {
+        acc[rem.rem_code] = rem
+        return acc
+      }, {} as Record<RemId, Rem>)
+    }),
+
+  pinRem: remId =>
+    set(state => ({
+      pinnedRems: new Set(state.pinnedRems).add(remId)
+    })),
+
+  unpinRem: remId =>
+    set(state => {
+      const newPinnedRems = new Set(state.pinnedRems)
+      newPinnedRems.delete(remId)
+      return { pinnedRems: newPinnedRems }
+    }),
+
+  isPinned: remId => get().pinnedRems.has(remId),
+
+  getAllRems: () => Object.values(get().rems),
+
+  getPinnedRems: () => {
+    const { rems, pinnedRems } = get()
+    return Array.from(pinnedRems)
+      .map(id => rems[id])
+      .filter(Boolean)
+  }
+}))
 
 export const useRems = () => {
   const store = useRemsStore()
